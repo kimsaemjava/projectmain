1330ff5394e13369cf132e9b4d799b62a7762166
ParkByeongsik <heaves06181223@gmail.com>
2024-06-12 22:08:21 +0900

    first

---
+++ src/main/java/com/example/projectmain/ProjectmainApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectmain/ProjectmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
